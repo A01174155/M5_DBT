5fdaf567e0bd8d12bad33a1ae82869f0a893f7bb
Mariana Ramirez <A01174155@tec.mx>
2024-10-15 22:31:37 +0000

    Ajustes en L2_DIM_CUSTOMER y L2_DIM_PRODUCT

--- models/marts/L2_DIM_PRODUCT.sql
+++ models/marts/L2_DIM_PRODUCT.sql
@@ -1,23 +1,6 @@
-WITH order_detail AS (
-    SELECT 
-        PRODUCT_ID
-    FROM {{ ref('L2_FCT_ORDER_DETAIL') }}
-),
-
-dim_product AS (
-    SELECT 
+SELECT 
     PRODUCT_ID,
     PRODUCT_NAME,
     PRODUCT_CATEGORY,
     PRODUCT_SUBCATEGORY
 FROM {{ ref('l1_menu') }}
-)
-
-SELECT
-    dp.PRODUCT_ID,
-    dp.PRODUCT_NAME,
-    dp.PRODUCT_CATEGORY,
-    dp.PRODUCT_SUBCATEGORY
-FROM order_detail od
-JOIN dim_product dp
-    ON od.PRODUCT_ID = dp.PRODUCT_ID
